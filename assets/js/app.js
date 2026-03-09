595ba356ff7e2b1fc529719aa5e78e3a91a016f1
Marine <marinekislova@gmail.com>
2026-03-09 17:23:01 +0200

    2026/03/09

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
 import * as commonFunctions from "./modules/functions.js";
 
-commonFunctions.isWebp();
+document.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  commonFunctions.isWebp();
+
+  document.querySelector(".burger").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".hamburger").classList.toggle("open");
+  });
+
+  //footer data
+  const footerData = document.getElementById("footerYear");
+  let currentData = new Date().getFullYear();
+  footerData.textContent = currentData;
+});
