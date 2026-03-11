c34fa4a4349dd9ae1e20553cb5f1a9108274191e
Marine <marinekislova@gmail.com>
2026-03-11 17:22:47 +0200

    2026/03/11

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".hamburger").classList.toggle("open");
   });
 
-  //tabs gallery
-  function tabs(selectorTab, selectorContent, selectorParent, activeClass) {
+  //tabs gallery and contact
+  function tabs(selectorTab, selectorContent, selectorParent, activeClass, display = "block") {
     const tabs = document.querySelectorAll(selectorTab);
     const tabsContent = document.querySelectorAll(selectorContent);
     const tabsParent = document.querySelector(selectorParent);
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function showTabContent(i) {
-      tabsContent[i].classList.add('tab-content-active');
-      tabsContent[i].style.display = "grid";
+      tabsContent[i].classList.add("tab-content-active");
+      tabsContent[i].style.display = display;
       tabs[i].classList.add(activeClass);
     }
 
@@ -55,10 +55,84 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  tabs(".tab-btn", ".tab-content", ".tab-nav", "tab-btn-active");
+  tabs(".tab-btn", ".tab-content", ".tab-nav", "tab-btn-active", "grid");
+  tabs(".contact__tab-btn", ".contact__content", ".contact__tabs", "contact-btn-active", "flex");
 
   //swiper
   swiperTestimornials.update();
+
+  // accordion
+
+  function initAccordion() {
+    const items = document.querySelectorAll(".accordion__item");
+
+    items.forEach((item) => {
+      const header = item.querySelector(".accordion-toggle");
+      const body = item.querySelector(".accordion__item-body");
+      const icon = item.querySelector(".accordion-icon");
+
+      header.addEventListener("click", () => {
+        const isOpen = item.classList.contains("active");
+
+        // 1. Закрываем все открытые элементы
+        items.forEach((el) => {
+          el.classList.remove("active");
+          el.querySelector(".accordion__item-body").style.display = "none";
+          el.querySelector(".accordion-icon").textContent = "+";
+        });
+
+        // 2. Если текущий не был открыт — открываем его
+        if (!isOpen) {
+          item.classList.add("active");
+          body.style.display = "block";
+          icon.textContent = "-";
+        }
+      });
+    });
+  }
+
+  // Запуск
+  initAccordion();
+
+  //отправка в телеграмм
+  function sendToTelegramm(formId, btnId, statusId) {
+    document.getElementById(formId).addEventListener("submit", async function (e) {
+      e.preventDefault();
+
+      const form = this;
+      const btn = document.getElementById(btnId);
+      const status = document.getElementById(statusId);
+      const formData = new FormData(form);
+
+      // Проверка ловушки (если поле заполнено — это бот)
+      if (formData.get("honey")) {
+        console.warn("Spam detected");
+        return;
+      }
+
+      btn.disabled = true;
+      status.textContent = "Отправка...";
+
+      try {
+        const response = await fetch("send.php", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          status.textContent = "Успешно отправлено!";
+          form.reset();
+        } else {
+          status.textContent = "Ошибка при отправке.";
+        }
+      } catch (error) {
+        status.textContent = "Ошибка сети.";
+      } finally {
+        btn.disabled = false;
+      }
+    });
+  }
+  sendToTelegramm("tg-form", "submit-btn", "status-message");
 
   //footer data
   const footerData = document.getElementById("footerYear");
--- assets/js/modules/swip.js
+++ assets/js/modules/swip.js
@@ -1,31 +1,55 @@
-
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 export const swiperTestimornials = new Swiper('[data-swiper="testimornialsSwiper"]', {
   modules: [Navigation, Pagination, Autoplay],
   // Optional parameters
   loop: true,
-  slidesPerView: 1,
   speed: 1000,
-  simulateTouch: false,
-  centeredSlides: false,
-  spaceBetween: 20,
+  simulateTouch: true,
+  centeredSlides: true,
+  spaceBetween: 10,
   roundLengths: true,
+
+  breakpoints: {
+    // Когда ширина экрана >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+
+    // Когда ширина экрана >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    // Когда ширина экрана >= 320px
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+    },
+  },
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
-    clickable: false,
+    clickable: true,
     dynamicBullets: true,
   },
 
-  // Autoplay
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-    stopOnLastSlide: false,
+  // If we need navination buttons
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
-  speed: 4000,
+  // Autoplay
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  //   stopOnLastSlide: false,
+  // },
+
+  // speed: 4000,
 });
